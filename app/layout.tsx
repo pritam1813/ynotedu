803857e9ceba9648c2ee630550625cbe1b1a2409
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2025-03-25 11:12:13 +0530

    Upgraded nextjs version and commented layout imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import "../public/assets/sass/styles.scss";
 import "../public/assets/sass/animations.scss";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import "react-calendar/dist/Calendar.css";
+// import { config } from "@fortawesome/fontawesome-svg-core";
+// import "@fortawesome/fontawesome-svg-core/styles.css";
+// import "react-calendar/dist/Calendar.css";
 import Header from "@/components/layout/headers/Header";
 import FooterOne from "@/components/layout/footers/FooterOne";
-config.autoAddCss = false;
+// config.autoAddCss = false;
 
 export default function RootLayout({
   children,
